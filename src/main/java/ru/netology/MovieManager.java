87ecbfcab012277ac28f16b92d2a2c16e97955c9
Elena Musinova <en.musinova@gmail.com>
2023-05-11 00:37:13 +0300

    FindLast is ready without tests

--- src/main/java/ru/netology/MovieManager.java
+++ src/main/java/ru/netology/MovieManager.java
@@ -2,7 +2,15 @@ package ru.netology;
 
 public class MovieManager {
     private String[] films = new String[0];
+    private int amount;
 
+    public MovieManager() {
+        this.amount = 5;
+    }
+
+    public MovieManager(int amount) {
+        this.amount = amount;
+    }
 
     public void addFilm(String filmName) {
         String[] tmp = new String[films.length + 1];
@@ -15,5 +23,20 @@ public class MovieManager {
 
     public String[] findAll() {
         return films;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if films.length < amount {
+            resultLength = films.length;
+        } else{
+            resultLength = amount;
+        }
+
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
     }
 }
